06b696372e58b8902c494e966adb22797de5ebc5
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-03-04 16:06:36 -0300

    Primeros cambios

---
+++ src/main/java/mc/nightmarephoenix/anchorsell/AnchorSell.java
@@ -0,0 +1,17 @@
+package mc.nightmarephoenix.anchorsell;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnchorSell extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
