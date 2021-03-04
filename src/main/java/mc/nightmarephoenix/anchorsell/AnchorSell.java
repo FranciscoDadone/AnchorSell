18cae65c831bb600f016064709acfd8f7f45d59a
Francisco Dadone <dadonefrancisco@hotmail.com.ar>
2021-03-04 20:43:40 -0300

    adding gui click event in main class

--- src/main/java/mc/nightmarephoenix/anchorsell/AnchorSell.java
+++ src/main/java/mc/nightmarephoenix/anchorsell/AnchorSell.java
@@ -2,6 +2,7 @@ package mc.nightmarephoenix.anchorsell;
 
 import mc.nightmarephoenix.anchorsell.commands.Anchor;
 import mc.nightmarephoenix.anchorsell.events.ActionAnchor;
+import mc.nightmarephoenix.anchorsell.events.MainAnchorEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AnchorSell extends JavaPlugin {
@@ -9,7 +10,8 @@ public final class AnchorSell extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new ActionAnchor(), this); // enabling the listener
+        getServer().getPluginManager().registerEvents(new ActionAnchor(), this);     // enabling the listener
+        getServer().getPluginManager().registerEvents(new MainAnchorEvents(), this); // gui click events
 
         getCommand("anchor").setExecutor(new Anchor(this));
     }
