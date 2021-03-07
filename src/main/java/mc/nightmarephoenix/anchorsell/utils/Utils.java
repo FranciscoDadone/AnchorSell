02f2cd50434a6c5fe30f50c6351c1dab3502e748
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-03-06 22:27:09 -0300

    Add anchor logic

--- src/main/java/mc/nightmarephoenix/anchorsell/utils/Utils.java
+++ src/main/java/mc/nightmarephoenix/anchorsell/utils/Utils.java
@@ -6,4 +6,12 @@ public class Utils {
     public static String Color(String str) {
         return ChatColor.translateAlternateColorCodes('§', str.replace("&", "§"));
     }
+
+    public static int getMoneyPerSecond(int anchorLevel) {
+        return (int) Math.round(0.1 * anchorLevel + Math.pow(anchorLevel, 0.8));
+    }
+
+    public static int getMoneyPerMinute(int anchorLevel) {
+        return (int) getMoneyPerSecond(anchorLevel) * 60;
+    }
 }
