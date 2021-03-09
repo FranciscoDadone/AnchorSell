3261ca59ffca09ae983729e8fbd796dff5b58d84
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-03-09 01:40:12 -0300

    mini-cambios

--- src/main/java/mc/nightmarephoenix/anchorsell/economy/EconomyManager.java
+++ src/main/java/mc/nightmarephoenix/anchorsell/economy/EconomyManager.java
@@ -24,19 +24,16 @@ public class EconomyManager {
     public static boolean withdrawFromUser(Player p, double balance) {
         EconomyResponse res = EconomyManager.getEconomy().withdrawPlayer(p, balance);
 
-        if (!res.equals(EconomyResponse.ResponseType.SUCCESS)) {
+        if (!res.equals(EconomyResponse.ResponseType.SUCCESS) && econ.getBalance(p) >= balance) {
             return true;
         } else {
             return false;
         }
     }
 
-
-
     public static Economy getEconomy() {
         return econ;
     }
-
 
     private static Economy econ;
 
