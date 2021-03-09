9ec7fa552ea2401aa34042530836fe083e851748
Francisco Dadone <dadonefrancisco@hotmail.com.ar>
2021-03-09 01:01:11 -0300

    starting with the economy system changed the user data storage from anchorsuuid to numbers

--- src/main/java/mc/nightmarephoenix/anchorsell/AnchorSell.java
+++ src/main/java/mc/nightmarephoenix/anchorsell/AnchorSell.java
@@ -1,22 +1,19 @@
 package mc.nightmarephoenix.anchorsell;
 
 import mc.nightmarephoenix.anchorsell.commands.AnchorCommand;
+import mc.nightmarephoenix.anchorsell.economy.EconomyManager;
 import mc.nightmarephoenix.anchorsell.events.*;
 import mc.nightmarephoenix.anchorsell.events.gui.GuiAnchorEvents;
-import net.milkbowl.vault.economy.Economy;
+import mc.nightmarephoenix.anchorsell.tasks.PayTask;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class AnchorSell extends JavaPlugin {
-
-//    public static File file;
-
     @Override
     public void onEnable() {
         // Vault check
-        if (!setupEconomy()) {
+        if (!EconomyManager.setupEconomy()) {
             this.getLogger().severe("Disabled due to no Vault dependency found!");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
@@ -29,37 +26,18 @@ public final class AnchorSell extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ActionAnchor(this), this);     // enabling the listener
         getServer().getPluginManager().registerEvents(new GuiAnchorEvents(this), this); // gui click events
         getServer().getPluginManager().registerEvents(new AnchorPlace(this), this);      // detects where the anchor has been placed and stores all the data
-        getServer().getPluginManager().registerEvents(new AnchorBreak(this), this);     // detects where the anchor has been remove and stores all the data
-        getServer().getPluginManager().registerEvents(new AnchorBlow(this), this);     // detects where the anchor has been remove and stores all the data
-
+        getServer().getPluginManager().registerEvents(new AnchorBreak(this), this);     // detects where the anchor has been removed and stores all the data
+        getServer().getPluginManager().registerEvents(new AnchorBlow(this), this);     // detects where the anchor has been blown and stores all the data
 
         // Commands
         this.getCommand("anchor").setExecutor(new AnchorCommand(this));
+
+        // Tasks
+        new PayTask(this, this.getConfig().getInt("pay-timer-in-minutes")).runTaskTimer(this, 0, 20 * this.getConfig().getInt("pay-timer-in-minutes") * 60); // starts the timer to pay the player
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
-
-
-    private boolean setupEconomy() {
-        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
-
-    public Economy getEconomy() {
-        return econ;
-    }
-
-
-    private Economy econ;
 }
---
+++ src/main/java/mc/nightmarephoenix/anchorsell/economy/EconomyManager.java
@@ -0,0 +1,40 @@
+package mc.nightmarephoenix.anchorsell.economy;
+
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class EconomyManager {
+
+    public static boolean setupEconomy() {
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+
+        RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static boolean depositToUser(Player p, double balance) {
+        EconomyResponse res = EconomyManager.getEconomy().bankDeposit(p.getName(), balance);
+        if(res.equals(EconomyResponse.ResponseType.SUCCESS)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+
+    private static Economy econ;
+
+}
