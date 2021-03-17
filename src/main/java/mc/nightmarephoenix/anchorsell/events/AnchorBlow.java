46d08fdb6e5b66a62f859e6ea97308d223ee83eb
Francisco Dadone <dadonefrancisco@hotmail.com.ar>
2021-03-17 16:03:42 -0300

    null pointer fixed in anchorblow and a bug

--- src/main/java/mc/nightmarephoenix/anchorsell/events/AnchorBlow.java
+++ src/main/java/mc/nightmarephoenix/anchorsell/events/AnchorBlow.java
@@ -21,6 +21,7 @@ public class AnchorBlow implements Listener {
 
     @EventHandler
     public void onExplosion(ExplosionPrimeEvent e) {
+        ArrayList<Block> anchors = new ArrayList<>();
         Entity entity = e.getEntity();
         int r = Math.round(e.getRadius());
         String radius = plugin.getConfig().getString("anchor.explotion-raius-break");
@@ -30,6 +31,9 @@ public class AnchorBlow implements Listener {
 
         for(Block b: possibleAnchors) {
             if(b.getType().equals(Material.RESPAWN_ANCHOR)) {
+
+                anchors.add(b);
+
                 b.breakNaturally(new ItemStack(Material.AIR));
                 world = entity.getLocation().getWorld();
 
@@ -45,9 +49,14 @@ public class AnchorBlow implements Listener {
         }
 
         Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> { // waiting 20 ticks to spawn the item because it can break during the explotion
-            int level = StorageManager.getAnchorLevel(plugin, loc);
-            world.dropItem(new Location(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ()), Utils.getAnchor(level, 1)).setInvulnerable(true);
-            StorageManager.anchorBreak(plugin, loc);
+            if(!anchors.isEmpty()) {
+                for(Block anchor: anchors) {
+                    int level = StorageManager.getAnchorLevel(plugin, anchor.getLocation());
+                    world.dropItem(new Location(anchor.getWorld(), anchor.getX(), anchor.getY(), anchor.getZ()), Utils.getAnchor(level, 1)).setInvulnerable(true);
+                    StorageManager.anchorBreak(plugin, anchor.getLocation());
+                }
+            }
+
         }, 20L);
     }
 
@@ -58,7 +67,6 @@ public class AnchorBlow implements Listener {
                 for(int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
                     if(new Location(location.getWorld(), x, y, z).getBlock().getType() == Material.RESPAWN_ANCHOR) {
                         blocks.add(location.getWorld().getBlockAt(x, y, z));
-                        loc = new Location(location.getWorld(), x, y, z);
                     }
                 }
             }
@@ -69,6 +77,4 @@ public class AnchorBlow implements Listener {
 
     private World world;
     private AnchorSell plugin;
-    private Location loc;
-
 }
