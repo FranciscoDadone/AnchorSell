1f206899c3481868424b98c7e90dee271e958239
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-06-26 23:37:43 -0300

    documented "Global"

--- src/main/java/mc/nightmarephoenix/anchorsell/hooks/Global.java
+++ src/main/java/mc/nightmarephoenix/anchorsell/hooks/Global.java
@@ -5,21 +5,36 @@ public class Global {
     private static WG wg;
     private static FactionsX fX;
 
+    /**
+     * Sets the world guard hook.
+     * @param value
+     */
     public static void setWorldGuard(boolean value) {
-        if(value)
-            wg = wg.ACTIVE;
-        else
-            wg = wg.NOT_ACTIVE;
+        if(value) wg = wg.ACTIVE;
+        else      wg = wg.NOT_ACTIVE;
     }
 
+    /**
+     * Sets the FactionsX hook.
+     * @param value
+     */
     public static void setFactionsX(boolean value) {
         if(value) fX = fX.ACTIVE;
         else      fX = fX.NOT_ACTIVE;
     }
 
+    /**
+     * Getter
+     * @return
+     */
     public static WG getWorldGuard() {
         return wg;
     }
+
+    /**
+     * Getter
+     * @return
+     */
     public static FactionsX getFactionsX() {
         return fX;
     }
