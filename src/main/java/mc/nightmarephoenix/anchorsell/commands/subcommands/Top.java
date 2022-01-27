f2400f16438aed23a0792d9db6c15a5d2f4b56a8
FranciscoDadone <dadonefran@gmail.com>
2022-01-27 10:31:26 -0300

    Updated plugin.yml Checked permission in top

--- src/main/java/mc/nightmarephoenix/anchorsell/commands/subcommands/Top.java
+++ src/main/java/mc/nightmarephoenix/anchorsell/commands/subcommands/Top.java
@@ -39,37 +39,39 @@ public class Top extends SubCommands {
     @SuppressWarnings("SuspiciousMethodCalls")
     @Override
     public void perform(CommandSender sender, String[] args) {
-        HashMap<String, Integer> top = StorageManager.getAnchorTop();
-        int page = 1;
-        double a = top.size();
-        double maxPages = Math.ceil(a / 10);
-        try {
-            if (Integer.parseInt(args[1]) > 0 && Integer.parseInt(args[1]) <= maxPages) {
-                page = Integer.parseInt(args[1]);
-            } else if (Integer.parseInt(args[1]) > maxPages) {
-                page = (int) maxPages;
-            }
-        } catch (Exception ignored) {}
+        if(sender.hasPermission(getPermission())) {
+            HashMap<String, Integer> top = StorageManager.getAnchorTop();
+            int page = 1;
+            double a = top.size();
+            double maxPages = Math.ceil(a / 10);
+            try {
+                if (Integer.parseInt(args[1]) > 0 && Integer.parseInt(args[1]) <= maxPages) {
+                    page = Integer.parseInt(args[1]);
+                } else if (Integer.parseInt(args[1]) > maxPages) {
+                    page = (int) maxPages;
+                }
+            } catch (Exception ignored) {}
 
-        int n = 1 + (10 * (page - 1));
-        boolean skipFirst = page != 1;
-        sender.sendMessage(Utils.Color("&7&m----------&r &5&lAnchor TOP &r&5&o(" + page + "/" + (int) maxPages + ") &7&m----------"));
-        for (int i = (top.size() - (10 * (page - 1))); i > (top.size() - (10 * page) - 1); i--) {
-            if (skipFirst) {
-                if ((top.size() - (10 * (page - 1))) != i) {
+            int n = 1 + (10 * (page - 1));
+            boolean skipFirst = page != 1;
+            sender.sendMessage(Utils.Color("&7&m----------&r &5&lAnchor TOP &r&5&o(" + page + "/" + (int) maxPages + ") &7&m----------"));
+            for (int i = (top.size() - (10 * (page - 1))); i > (top.size() - (10 * page) - 1); i--) {
+                if (skipFirst) {
+                    if ((top.size() - (10 * (page - 1))) != i) {
+                        try {
+                            sender.sendMessage(Utils.Color("&7#" + n + " &b" + Bukkit.getOfflinePlayer(UUID.fromString(top.keySet().toArray()[i].toString())).getName() + "&7 - &f" + top.get(top.keySet().toArray()[i])));
+                            n++;
+                        } catch (Exception ignored) {
+                        }
+                    }
+                } else {
                     try {
                         sender.sendMessage(Utils.Color("&7#" + n + " &b" + Bukkit.getOfflinePlayer(UUID.fromString(top.keySet().toArray()[i].toString())).getName() + "&7 - &f" + top.get(top.keySet().toArray()[i])));
                         n++;
-                    } catch (Exception ignored) {
-                    }
+                    } catch (Exception ignored) {}
                 }
-            } else {
-                try {
-                    sender.sendMessage(Utils.Color("&7#" + n + " &b" + Bukkit.getOfflinePlayer(UUID.fromString(top.keySet().toArray()[i].toString())).getName() + "&7 - &f" + top.get(top.keySet().toArray()[i])));
-                    n++;
-                } catch (Exception ignored) {}
             }
-        }
-        sender.sendMessage(Utils.Color("&7&m-------------------------------------"));
+            sender.sendMessage(Utils.Color("&7&m-------------------------------------"));
+        } else Utils.noPermission(getPermission(), sender);
     }
 }
